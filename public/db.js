b126800ed5500cde1246e1e05d7e0895e199da77
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-22 16:09:39 -0600

    tweak directory routes.

--- public/db.js
+++ public/db.js
@@ -1,4 +1,4 @@
-import { response } from "express";
+// import { response } from "express";
 
 const indexedDb =
     window.indexedDB ||
@@ -38,10 +38,8 @@ function saveRecord(record) {
     // create a transaction on the pending db with readwrite access
     const transaction = db.transaction(["pending"], "readwrite");
 
-    // access your pending object store
     const store = transaction.objectStore("pending");
 
-    // add record to your store with add method.
     store.add(record);
 };
 
