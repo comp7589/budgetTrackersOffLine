fcef94c63d7e780e0201d270f2a02544f3c1377d
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-24 15:34:30 -0600

    add connection ware.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+var MONGODB_URI = process.env.MONGODB_URI || "monogodb://localhost/budget";
 
 const PORT = 3000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
